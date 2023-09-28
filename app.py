702da881e0e0b4243e8a48fa4d77ff280a484a95
ryo-tsubota <ryo-tsubota@dac.co.jp>
2023-09-28 17:58:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 # Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/config.json')
+def config():
+    with open('./config.json') as f:
+        jdata = json.load(f)
+    return jsonify(jdata)
 
 # アプリケーションを実行
 if __name__ == '__main__':
