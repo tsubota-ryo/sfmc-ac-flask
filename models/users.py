d20899e5958cf821f669eb8383948ffd00d999de
ryo-tsubota <ryo-tsubota@dac.co.jp>
2023-10-25 17:29:49 +0900

    Update users.py

--- models/users.py
+++ models/users.py
@@ -6,25 +6,27 @@ import google.cloud.logging
 
 class Users():
     
-    def __init__(self,data):
+    def __init__(self,db, data):
         self.logger = logging.getLogger('weblog').getChild("uers")
         self.logger.setLevel(logging.DEBUG)
         client = google.cloud.logging.Client()
         client.setup_logging()
         self.logger.info("init start")
-        # self.db = firestore.Client()
-        
+        self.db = db
         self.data = data
 
 
     def insert(self,collection_name):
-        self.logger.info(collection_name)
-        self.logger.info(self.data)
-        self.db = firestore.Client()
+
         try:
+            self.logger.info(collection_name)
+            self.logger.info(self.data)
+
             self.db.collection(collection_name).document().set(self.data)
+
         except Exception as e:
             self.logger.error(e)
+            raise
         
     # TODO:validation check
     # def validation_data(self,data):
